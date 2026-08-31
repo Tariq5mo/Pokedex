fbe227b2cae8a221333270b4cc26b8f7278bf3e3
Tariq5mo <tariqomer756@gmail.com>
2026-08-31 14:44:48 +0200

    feat: enhance REPL functionality with command handling and improved input processing

--- src/repl.ts
+++ src/repl.ts
@@ -1,22 +1,35 @@
 import { createInterface } from "readline";
+import { commandExit } from "./command_exit.js";
+import { commandHelp } from "./src/command_help.js";
+import { CLICommand, initState, State } from "./state.js";
 
-export function startREPL() {
-	const rl = createInterface({ input: process.stdin, output: process.stdout, prompt: "Pokedex > "})
-	rl.prompt()
+export function startREPL(state: State) {
+	const rl = state.readline
+	rl.prompt();
 	rl.on("line", (input) => {
 		const args = cleanInput(input);
 		if (args.length < 1) {
 			rl.prompt()
 			return;
 		}
-		console.log(`Your command was: ${args[0]}`);
+		const userCommand = args[0];
+		const commands = state.commands
+		if (commands[userCommand]) {
+			try {
+				commands[userCommand].callback(state)
+			} catch (err) {
+				if (err instanceof Error) console.log(err);
+			}
+		} else {
+			console.log("Unknown command");
+		}
 		rl.prompt();
 	})
 }
 
 export function cleanInput(input: string): string[] {
 	let valStr = input.trim();
-	const arr =  valStr.split(" ").filter((value) => value != "");
+	const arr = valStr.split(" ").filter((value) => value != "");
 	arr.forEach((str, index) => {
 		arr[index] = arr[index].trim().toLowerCase();
 	});
