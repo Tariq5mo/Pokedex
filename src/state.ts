ded8197cb607d091acb06b0b6020c889b77c1ac4
Tariq5mo <tariqomer756@gmail.com>
2026-08-31 20:03:05 +0200

    feat: update State type to include PokeAPIObject and location URLs, and modify callback type to return a Promise

--- src/state.ts
+++ src/state.ts
@@ -2,20 +2,27 @@ import { createInterface, type Interface } from "readline";
 import { cleanInput } from "./repl.js";
 import { commandExit } from "./command_exit.js";
 import { commandHelp } from "./src/command_help.js";
+import { PokeAPI } from "./pokeapi.js";
+import { commandMap } from "./command_map.js";
+import { commandMapb } from "./command_mapb.js";
 
 export type CLICommand = {
 	name: string;
 	description: string;
-	callback: (state: State) => void;
+	callback: (state: State) => Promise<void>;
 };
 
 export type State = {
 	readline: Interface,
-	commands: Record<string, CLICommand>
+	commands: Record<string, CLICommand>,
+	PokeAPIObject: PokeAPI,
+	nextLocationsURL: string | null,
+	prevLocationsURL: string | null,
 }
 
 export function initState(): State {
 	const rl = createInterface({ input: process.stdin, output: process.stdout, prompt: "Pokedex > " })
+	const pokeAPIObject = new PokeAPI()
 	return {
 		readline: rl,
 		commands: {
@@ -28,7 +35,20 @@ export function initState(): State {
 				name: "help",
 				description: "Displays a help message",
 				callback: commandHelp,
+			},
+			map: {
+				name: "map",
+				description: "Displays the names of 20 location areas in the Pokemon world",
+				callback: commandMap,
+			},
+			mapb: {
+				name: "mapb",
+				description: "Displays the previous names of 20 location areas in the Pokemon world",
+				callback: commandMapb,
 			}
-		}
+		},
+		PokeAPIObject: pokeAPIObject,
+		nextLocationsURL: null,
+		prevLocationsURL: null
 	};
 }
