a72a9d6cf0284e89abdb75f65acbcb0e0c29b095
Tariq5mo <tariqomer756@gmail.com>
2026-08-31 11:27:16 +0200

    Add repl start

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,19 @@
+import { createInterface } from "readline";
+
+export function startREPL() {
+	const rl = createInterface({ input: process.stdin, output: process.stdout, prompt: "Pokedex > "})
+	rl.prompt()
+	rl.on("line", (input) => {
+		const args = cleanInput(input);
+		if (args.length < 1) {
+			rl.prompt()
+			return;
+		}
+		console.log(`Your command was: ${args[0]}`);
+		rl.prompt();
+	})
+}
+
 export function cleanInput(input: string): string[] {
 	let valStr = input.trim();
 	const arr =  valStr.split(" ").filter((value) => value != "");
