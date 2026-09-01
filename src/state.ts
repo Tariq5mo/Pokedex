e27bca4f29e05b71e8f6c0684889d24fc817d6ba
Tariq5mo <tariqomer756@gmail.com>
2026-09-01 17:10:15 +0200

    feat: update CLICommand to accept additional arguments and add explore command

--- src/state.ts
+++ src/state.ts
@@ -5,11 +5,12 @@ import { commandHelp } from "./command_help.js";
 import { PokeAPI } from "./pokeapi.js";
 import { commandMap } from "./command_map.js";
 import { commandMapb } from "./command_mapb.js";
+import { commandExplore } from "./command_explore.js";
 
 export type CLICommand = {
 	name: string;
 	description: string;
-	callback: (state: State) => Promise<void>;
+	callback: (state: State, ...args: string[]) => Promise<void>;
 };
 
 export type State = {
@@ -45,7 +46,12 @@ export function initState(): State {
 				name: "mapb",
 				description: "Displays the previous names of 20 location areas in the Pokemon world",
 				callback: commandMapb,
-			}
+			},
+			explore: {
+				name: "explore",
+				description: "Displays a list of all the Pokémon in a given area.",
+				callback: commandExplore,
+			},
 		},
 		PokeAPIObject: pokeAPIObject,
 		nextLocationsURL: null,
